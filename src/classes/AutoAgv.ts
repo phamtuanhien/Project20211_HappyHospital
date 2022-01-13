69eae11d974d84864d1912d87b9f1b48c8629337
Tuan Hien <986753421hn@gmail.com>
2022-01-13 22:13:07 +0700

    fixed lỗi di chuyển quỹ đạo kì dị của agv tại một số trường hợp #26

--- src/classes/AutoAgv.ts
+++ src/classes/AutoAgv.ts
@@ -16,7 +16,7 @@ export class AutoAgv extends Actor {
   public waitT: number;
   public sobuocdichuyen: number;
   public thoigiandichuyen: number;
-  public hybridState : HybridState | undefined;
+  public hybridState: HybridState | undefined;
   public endX: number;
   public endY: number;
   public firstText?: Text;
@@ -33,10 +33,10 @@ export class AutoAgv extends Actor {
     graph: Graph
   ) {
     super(scene, x * 32, y * 32, "agv");
-    this.startX = x*32;
-    this.startY = y*32;
-    this.endX = endX*32;
-    this.endY = endY*32;
+    this.startX = x * 32;
+    this.startY = y * 32;
+    this.endX = endX * 32;
+    this.endY = endY * 32;
 
     this.graph = graph;
     this.getBody().setSize(32, 32);
@@ -46,11 +46,18 @@ export class AutoAgv extends Actor {
     this.curNode = this.graph.nodes[x][y];
     this.curNode.setState(StateOfNode2D.BUSY);
     this.endNode = this.graph.nodes[endX][endY];
-    this.firstText = new Text(this.scene, endX * 32, endY * 32, "DES", "16px", "#F00");
+    this.firstText = new Text(
+      this.scene,
+      endX * 32,
+      endY * 32,
+      "DES",
+      "16px",
+      "#F00"
+    );
     this.path = this.calPathAStar(this.curNode, this.endNode);
     this.sobuocdichuyen = 0;
     this.thoigiandichuyen = performance.now();
-    this.estimateArrivalTime(x*32, y*32, endX*32, endY*32);
+    this.estimateArrivalTime(x * 32, y * 32, endX * 32, endY * 32);
     this.hybridState = new RunningState();
   }
 
@@ -59,7 +66,6 @@ export class AutoAgv extends Actor {
     // console.log(this.x, this.y);
     this.hybridState?.move(this);
   }
-
 
   private heuristic(node1: Node2D, node2: Node2D): number {
     return Math.abs(node1.x - node2.x) + Math.abs(node1.y - node2.y);
@@ -166,10 +172,14 @@ export class AutoAgv extends Actor {
     return false;
   }
 
-  public changeTarget() : void {
+  public changeTarget(): void {
     var mainScene = this.scene as MainScene;
-    let agvsToGate1 : Array<number> = mainScene.mapOfExits.get("Gate1") as Array<number>;
-    let agvsToGate2 : Array<number> = mainScene.mapOfExits.get("Gate2") as Array<number>;
+    let agvsToGate1: Array<number> = mainScene.mapOfExits.get(
+      "Gate1"
+    ) as Array<number>;
+    let agvsToGate2: Array<number> = mainScene.mapOfExits.get(
+      "Gate2"
+    ) as Array<number>;
     var choosenGate = agvsToGate1[2] < agvsToGate2[2] ? "Gate1" : "Gate2";
     var newArray = mainScene.mapOfExits.get(choosenGate) as Array<number>;
     newArray[2]++;
@@ -178,19 +188,32 @@ export class AutoAgv extends Actor {
     this.startX = this.endX;
     this.startY = this.endY;
 
-    var xEnd : number = newArray[0];
-    var yEnd : number = newArray[1];
+    var xEnd: number = newArray[0];
+    var yEnd: number = newArray[1];
 
-    this.endX = xEnd*32;
-    this.endY = yEnd*32;
+    this.endX = xEnd * 32;
+    this.endY = yEnd * 32;
 
     var finalAGVs = (mainScene.mapOfExits.get(choosenGate) as Array<number>)[2];
 
     this.endNode = this.graph.nodes[xEnd][yEnd];
-    this.firstText = new Text(this.scene, xEnd * 32, yEnd * 32, "DES_" + finalAGVs, "16px", "#F00");
+    this.firstText = new Text(
+      this.scene,
+      xEnd * 32,
+      yEnd * 32,
+      "DES_" + finalAGVs,
+      "16px",
+      "#F00"
+    );
     this.path = this.calPathAStar(this.curNode, this.endNode);
+    this.cur = 0;
     this.sobuocdichuyen = 0;
     this.thoigiandichuyen = performance.now();
-    this.estimateArrivalTime(32*this.startX, 32*this.startY, this.endX*32, this.endY*32);
+    this.estimateArrivalTime(
+      32 * this.startX,
+      32 * this.startY,
+      this.endX * 32,
+      this.endY * 32
+    );
   }
 }
